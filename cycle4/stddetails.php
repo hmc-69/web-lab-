97210297b6b4d8fba37cef551ea029f091ff0570
hmc-69 <hemanthchandran541@gmail.com>
2025-12-28 08:40:00 +0530

    changes

--- cycle4/stddetails.php
+++ cycle4/stddetails.php
@@ -4,7 +4,7 @@
 </head>
 <body align="center">
 <h2>Details of Student</h2>
-<form method="REQUEST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="REQUEST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
 Name: <input type="text" name="name"><br><br>
 Email id: <input type="email" name="mail"><br><br>
 Address: <textarea rows="4" name="adrs"></textarea><br><br>
@@ -12,23 +12,26 @@ Gender: M <input type="radio" value="M" name="gender"> F <input type="radio" val
 DOB: <input type="date" name="dob"><br><br>
 <input type="submit" value="Submit">
 </form>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-if(isset($_REQUEST["name"],$_REQUEST["mail"], $_REQUEST["adrs"],
-$_REQUEST["gender"], $_REQUEST["dob"])) {
-$name = $_REQUEST["name"];
-$mail = $_REQUEST["mail"];
-$adrs = $_REQUEST["adrs"];
-$gen = $_REQUEST["gender"];
-$dob = $_REQUEST["dob"];
-echo
-"<br><br>DETAILS:- <br><br>"; echo "Name: $name <br> Email ID: $mail <br>
-Address: $adrs <br> Gender: $gen <br> D.O.B: $dob" ;
-}
-else {
-echo "<p>Please enter values..</p>";
-}
-}
-?>
+<?php if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (
+        isset(
+            $_REQUEST["name"],
+            $_REQUEST["mail"],
+            $_REQUEST["adrs"],
+            $_REQUEST["gender"],
+            $_REQUEST["dob"]
+        )
+    ) {
+        $name = $_REQUEST["name"];
+        $mail = $_REQUEST["mail"];
+        $adrs = $_REQUEST["adrs"];
+        $gen = $_REQUEST["gender"];
+        $dob = $_REQUEST["dob"];
+        echo "<br><br>DETAILS:- <br><br>";
+        echo "Name: $name <br> Email ID: $mail <br>Address: $adrs <br> Gender: $gen <br> D.O.B: $dob";
+    } else {
+        echo "<p>Please enter values..</p>";
+    }
+} ?>
 </body>
 </html>
